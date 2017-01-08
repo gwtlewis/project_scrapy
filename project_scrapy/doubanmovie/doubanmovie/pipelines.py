5d1d62cbb83b2d84e4e068f34624c09493e2b21e
gwtlewis <gwtlewis@qq.com>
2017-01-08 10:37:29 +0800

    new way to connect to DB

--- project_scrapy/doubanmovie/doubanmovie/pipelines.py
+++ project_scrapy/doubanmovie/doubanmovie/pipelines.py
@@ -8,17 +8,21 @@ import pymongo
 from scrapy.conf import settings
 from scrapy import log
 from items import DoubanmovieItem
-
+#
 
 class DoubanmoviePipeline(object):
     def __init__(self):
-        connection = pymongo.MongoClient(
-            settings['MONGODB_SERVER'],
-            settings['MONGODB_PORT']
-        )
-        connection.admin.authenticate(settings['MONGODB_USER'], settings['MONGODB_PSW'])
-        db = connection[settings['MONGODB_DB']]
-        self.collection = db[settings['MONGODB_COLLECTION']]
+        #connection = pymongo.MongoClient(
+        #    settings['MONGODB_SERVER'],
+        #    settings['MONGODB_PORT']
+        #)
+        #connection.admin.authenticate(settings['MONGODB_USER'], settings['MONGODB_PSW'])
+        #db = connection[settings['MONGODB_DB']]
+        #self.collection = db[settings['MONGODB_COLLECTION']]
+        self.client = pymongo.MongoClient(host=settings['MONGODB_HOST'], port=settings['MONGODB_PORT'])
+        self.client.admin.authenticate(settings['MONGODB_USER'], settings['MONGODB_PSW'])
+        self.db = self.client[settings['MONGODB_DB']]
+        self.collection = self.db[settings['MONGODB_COLLECTION']]
 
     def process_item(self, item, spider):
         movie = dict(item)
